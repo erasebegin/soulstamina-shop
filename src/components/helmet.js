e5131b5b01d7f76b2f573249b754da9390c48890
erasebegin <chris.james.haupt@gmail.com>
2020-10-22 20:09:30 +0100

    fix build bug 2

--- src/components/helmet.js
+++ src/components/helmet.js
@@ -25,7 +25,7 @@ export default ({ title }) => (
         />
         <meta name="description" content={data.site.siteMetadata.description} />
         <meta name="keywords" content={data.site.siteMetadata.keywords} />
-        <title>{title ? title.toLowerCase() : siteMetadata.title}</title>
+        <title>{title ? title.toLowerCase() : data.site.siteMetadata.title}</title>
         <html lang="en" />
         {/* Google / Search Engine Meta Tags */}
         <meta itemprop="name" content={data.site.siteMetadata.author} />
